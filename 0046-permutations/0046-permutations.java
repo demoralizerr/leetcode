eafc2a1f34bf6e3f1ace1c089d67c834a66205a5
kartik patel <kartikpatell4july1997@gmail.com>
2026-01-06 17:43:17 +0530

    Time: 2 ms (39.86%), Space: 45.7 MB (30.76%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,34 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        backtrack(list, new ArrayList<>(), nums);
-        return list;
-
+        List<List<Integer>> res = new ArrayList<>();
+        int[] selected = new int[nums.length];
+        backtrack(new ArrayList<>(), nums, res,selected);
+        return res;
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
-        if (tempList.size() == nums.length) {
-            list.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (tempList.contains(nums[i]))
-                    continue; // element already exists, skip
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums);
-                tempList.remove(tempList.size() - 1);
-            }
-        }
-    }
 
+    private void backtrack(List<Integer> state, int[] nums, List<List<Integer>> res,int[] selected){
+        
+         if(state.size()==nums.length){
+              res.add(new ArrayList<>(state));
+              return;
+         }
+
+         for(int i =0; i<nums.length;i++){
+             //Pruning
+             if(selected[i]==0){
+                selected[i] = 1;
+                state.add(nums[i]);
+            
+                backtrack(state,nums,res,selected);
+               
+               // Backtrack
+                selected[i] = 0;
+                state.remove(state.size()-1);
+             }
+
+         }
+
+
+    }
 }
