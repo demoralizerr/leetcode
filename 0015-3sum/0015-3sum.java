062df25c4bc7327cd88ee02b5a8bb754c0172537
kartik patel <kartikpatell4july1997@gmail.com>
2026-01-10 10:54:31 +0530

    Time: 869 ms (11.23%), Space: 60.2 MB (15.48%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,30 @@
-public class Solution {
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n= nums.length;
+        Set<List<Integer>> set = new HashSet<>();
         Arrays.sort(nums);
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : nums) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
+
+        for(int i=0;i<n-2;i++){
+         int l=i+1; int r=n-1;
+         while(l<r){
+             int sum = nums[l]+nums[r]+nums[i];
+
+             if(sum==0){
+                set.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                l++;
+                r--;
+             }
+             else if(sum<0){
+                l++;
+             }
+             else
+             r--;
+
+
+         }
+
+
         }
-
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            count.put(nums[i], count.get(nums[i]) - 1);
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            for (int j = i + 1; j < nums.length; j++) {
-                count.put(nums[j], count.get(nums[j]) - 1);
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int target = -(nums[i] + nums[j]);
-                if (count.getOrDefault(target, 0) > 0) {
-                    res.add(Arrays.asList(nums[i], nums[j], target));
-                }
-            }
-
-            for (int j = i + 1; j < nums.length; j++) {
-                count.put(nums[j], count.get(nums[j]) + 1);
-            }
-        }
-        return res;
+        return new ArrayList<>(set);
     }
 }
