30b159b7f2257133056850a75a145f6c77bc8272
kartik patel <kartikpatell4july1997@gmail.com>
2026-01-21 21:47:24 +0530

    Time: 0 ms (100%), Space: 43.9 MB (54.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,23 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        //find pivot index
-        int pivot = pivot(nums, 0, n - 1);
 
-        //do binary search on right side of pivot
-        int rans = binarysearch(nums, pivot, n - 1, target);
-        if (rans != -1)
-            return rans;
+        // Find pivot index (index of minimum element)
+        int pivot = findPivot(nums);
 
-        //do binary search on left side of pivot
-        int lans = binarysearch(nums, 0, pivot-1, target);
-        if (lans != -1)
-            return lans;
+        // Search in right sorted portion
+        int result = binarySearch(nums, pivot, n - 1, target);
+        if (result != -1)
+            return result;
 
-        return -1;
+        // Search in left sorted portion
+        return binarySearch(nums, 0, pivot - 1, target);
     }
 
-    private int binarysearch(int[] nums, int low, int high, int target) {
+    private int binarySearch(int[] nums, int low, int high, int target) {
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] == target)
@@ -30,17 +27,22 @@ class Solution {
         return -1;
     }
 
-    //finds the index of element which is highest in rotated array(peak)
-    private int pivot(int[] nums, int low, int high) {
+    // Find index of minimum element (rotation point)
+    private int findPivot(int[] nums) {
+        int low = 0, high = nums.length - 1;
+
         while (low < high) {
             int mid = low + (high - low) / 2;
+
             // If mid element is greater than last element,
             // minimum must be in right half
-            if (nums[mid] > nums[high])
-                low = mid+1;
-            else
-                high = mid; // Otherwise, minimum is in left half (including mid)
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                // Otherwise, minimum is in left half (including mid)
+                high = mid;
+            }
         }
-        return low; // or high both are equal
+        return low; // or high, they're equal
     }
 }
