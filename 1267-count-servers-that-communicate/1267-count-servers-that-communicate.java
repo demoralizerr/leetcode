aca74be4fa9c8c50efe0c09b1a0829faa7fc0ec4
kartik patel <kartikpatell4july1997@gmail.com>
2026-02-14 12:12:42 +0530

    Time: 2 ms (100%), Space: 55.2 MB (73.75%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -4,24 +4,23 @@ public class Solution {
         int[] row_cnt = new int[ROWS];
         int[] col_cnt = new int[COLS];
 
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (grid[r][c] == 1) {
-                    row_cnt[r]++;
-                    col_cnt[c]++;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (grid[i][j] == 1) {
+                    row_cnt[i]++;
+                    col_cnt[j]++;
                 }
             }
         }
 
-        int res = 0;
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (grid[r][c] == 1 && Math.max(row_cnt[r], col_cnt[c]) > 1) {
-                    res++;
-                }
+        int count = 0;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (grid[i][j] == 1 && Math.max(row_cnt[i], col_cnt[j]) > 1)
+                    count++;
             }
         }
 
-        return res;
+        return count;
     }
 }
