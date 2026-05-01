0b91cb6f75923d2f4b6f46c11047856b5313c582
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-01 09:30:47 +0530

    Time: 1 ms (37.95%), Space: 45 MB (23.41%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,17 +1,37 @@
 class Solution {
+
+    // recursive solution using reccurrence relation
+    public int solve(int[] cost, int n) {
+        //base case
+        if (n == 0)
+            return cost[0];
+        if (n == 1)
+            return cost[1];
+
+        int ans = cost[n] + Math.min(solve(cost, n - 1), solve(cost, n - 2));
+        return ans;
+    }
+
+    // dp + memoization solution
+    public int solve2(int[] cost, int n,int[] dp){
+        //base case
+        if (n == 0)
+            return cost[0];
+        if (n == 1)
+            return cost[1];
+
+         if(dp[n]!=-1)
+            return dp[n];   
+
+         dp[n] = cost[n] + Math.min(solve2(cost, n - 1,dp), solve2(cost, n - 2,dp));  
+         return dp[n]; 
+
+    }
+
     public int minCostClimbingStairs(int[] cost) {
-        // min cost to reach n = cost of stepping on current i + min cost to reach (n-1 or n-2)
-        // dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
-        int prev1 = cost[0];
-        int prev2 = cost[1];
-
-        for(int i=2;i<cost.length;i++){
-            int curr = cost[i] + Math.min(prev1,prev2);
-            prev1 = prev2;
-            prev2= curr;
-        }
-
-        return Math.min(prev1,prev2);
-        
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return Math.min(solve2(cost,n-1,dp),solve2(cost,n-2,dp));
     }
 }
