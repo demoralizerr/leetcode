8fdf03751ca6445a8caa91b24e3131533e3cf218
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-02 09:17:30 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.32%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    //DP + memoization
+    public int solve(int[] nums, int idx, int[] dp) {
+        //base case
+        if (idx >= nums.length)
+            return 0;
+
+        if (dp[idx] != -1)
+            return dp[idx];
+
+        //pick
+        int pick = nums[idx] + solve(nums, idx + 2, dp);
+        //notpick
+        int notpick = solve(nums, idx + 1, dp);
+
+        dp[idx] = Math.max(pick, notpick);
+        return dp[idx];
+    }
+
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0, dp);
+    }
+}
