00ab3a6b78c2eb370d26022c89a46e2c69eddff0
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-02 09:30:32 +0530

    Time: 0 ms (100%), Space: 43 MB (5.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -17,10 +17,27 @@ class Solution {
         dp[idx] = Math.max(pick, notpick);
         return dp[idx];
     }
+    
+    //Bottom up DP
+    public int solveBottomUp(int[] nums){
+    //let dp[i] be max money we can rob till house i
+    
+       int n = nums.length;
+
+       if (n == 1) return nums[0];
+       int[] dp = new int[n+1];
+       dp[0] = nums[0];
+       dp[1] = Math.max(nums[0], nums[1]);
+
+       for(int i=2;i<n;i++){
+          dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
+       }
+         return dp[n-1];
+    }
 
     public int rob(int[] nums) {
         int[] dp = new int[nums.length + 1];
         Arrays.fill(dp, -1);
-        return solve(nums, 0, dp);
+        return solveBottomUp(nums);
     }
 }
