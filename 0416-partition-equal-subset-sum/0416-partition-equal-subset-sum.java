ae0bcd9f0629474235b003bad067fd7f3fc5aff1
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-13 20:56:54 +0530

    Time: 54 ms (40.11%), Space: 81.3 MB (8.8%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -10,7 +10,32 @@ class Solution {
 
         Boolean[][] dp = new Boolean[n + 1][sum / 2 + 1];
 
-        return canPartition(nums, sum / 2, 0, dp);
+        // return canPartition(nums, sum / 2, 0, dp);
+        return canPartitionTabulation(nums, sum / 2);
+    }
+
+    public boolean canPartitionTabulation(int[] nums, int sum) {
+        int n = nums.length;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int summ = 1; summ <= sum; summ++) {
+                boolean notpick = dp[index + 1][summ];
+                boolean pick = false;
+                if (summ - nums[index] >= 0) {
+                    pick = dp[index+1][summ-nums[index]];
+                }
+
+                dp[index][summ] = pick || notpick;
+            }
+        }
+
+        return dp[0][sum];
+
     }
 
     public boolean canPartition(int[] nums, int sum, int index, Boolean[][] dp) {
