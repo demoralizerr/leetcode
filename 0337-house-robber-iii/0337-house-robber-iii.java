d6db925c68c5cc8e4df19fd5fe5676f0a9294c2c
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-13 21:35:51 +0530

    Time: 0 ms (100%), Space: 46.5 MB (72.22%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -16,7 +16,28 @@
 class Solution {
     public int rob(TreeNode root) {
         HashMap<TreeNode, Integer> dp = new HashMap<>();
-        return solve(root, dp);
+        int[] bottomUpState = new int[2];
+        bottomUpState = robTabulation(root);
+        return Math.max(bottomUpState[0], bottomUpState[1]);//[notpicked, picked]
+    }
+
+    public int[] robTabulation(TreeNode root) {
+
+        if (root == null)
+            return new int[] { 0, 0 };
+        int[] left = robTabulation(root.left);
+        int[] right = robTabulation(root.right);
+
+        int[] currdp = new int[2];
+
+        //notpicked state
+        currdp[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        //picked state
+        currdp[1] = root.val + left[0] + right[0];
+
+        return currdp;
+
     }
 
     public int solve(TreeNode root, HashMap<TreeNode, Integer> dp) {
