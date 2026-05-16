e41b03ed0b1768513d58fe57aa57a814d4524c07
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-16 10:52:03 +0530

    Time: 0 ms (100%), Space: 42.9 MB (18.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,7 +3,26 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        return solve(nums, 0,dp);
+        // return solve(nums, 0, dp);
+
+        return solveTabulation(nums);
+    }
+
+    public int solveTabulation(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+
+        dp[0] = nums[0];
+        if (n == 1)
+            return dp[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int index = 2; index < n; index++) {
+            int rob = nums[index] + dp[index - 2];
+            int notrob = dp[index - 1];
+            dp[index] = Math.max(rob, notrob);
+        }
+        return dp[n - 1];
     }
 
     public int solve(int[] nums, int index, int[] dp) {
@@ -11,13 +30,13 @@ class Solution {
         if (index >= nums.length)
             return 0;
 
-         if(dp[index]!=-1)
-            return dp[index];   
+        if (dp[index] != -1)
+            return dp[index];
 
-        int rob = nums[index] + solve(nums, index + 2,dp);
-        int notrob = solve(nums, index + 1,dp);
+        int rob = nums[index] + solve(nums, index + 2, dp);
+        int notrob = solve(nums, index + 1, dp);
 
-        dp[index] =  Math.max(rob, notrob);
+        dp[index] = Math.max(rob, notrob);
         return dp[index];
     }
 }
