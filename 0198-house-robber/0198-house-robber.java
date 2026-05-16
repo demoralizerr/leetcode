c409c391375251bc4a410991e8e4fb18ed9d3c81
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-16 10:47:18 +0530

    Time: 0 ms (100%), Space: 42.9 MB (18.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,43 +1,23 @@
 class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0,dp);
+    }
 
-    //DP + memoization
-    public int solve(int[] nums, int idx, int[] dp) {
-        //base case
-        if (idx >= nums.length)
+    public int solve(int[] nums, int index, int[] dp) {
+
+        if (index >= nums.length)
             return 0;
 
-        if (dp[idx] != -1)
-            return dp[idx];
+         if(dp[index]!=-1)
+            return dp[index];   
 
-        //pick
-        int pick = nums[idx] + solve(nums, idx + 2, dp);
-        //notpick
-        int notpick = solve(nums, idx + 1, dp);
+        int rob = nums[index] + solve(nums, index + 2,dp);
+        int notrob = solve(nums, index + 1,dp);
 
-        dp[idx] = Math.max(pick, notpick);
-        return dp[idx];
-    }
-    
-    //Bottom up DP
-    public int solveBottomUp(int[] nums){
-    //let dp[i] be max money we can rob till house i
-    
-       int n = nums.length;
-
-       if (n == 1) return nums[0];
-       int[] dp = new int[n+1];
-       dp[0] = nums[0];
-       dp[1] = Math.max(nums[0], nums[1]);
-
-       for(int i=2;i<n;i++){
-          dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
-       }
-         return dp[n-1];
-    }
-
-    public int rob(int[] nums) {
-        int[] dp = new int[nums.length + 1];
-        Arrays.fill(dp, -1);
-        return solveBottomUp(nums);
+        dp[index] =  Math.max(rob, notrob);
+        return dp[index];
     }
 }
