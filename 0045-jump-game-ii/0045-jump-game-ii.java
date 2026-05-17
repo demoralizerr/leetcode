fa40b3516835b1b4bb75b2247a6f3a4a58528f26
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-17 11:07:29 +0530

    Time: 34 ms (14.99%), Space: 46 MB (99.89%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,7 +3,33 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        return solve(nums, 0, dp);
+      //  return solve(nums, 0, dp);
+      return solveTabulation(nums);
+    }
+
+    public int solveTabulation(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+
+        if (n == 1)
+            return 0;
+
+        dp[n - 1] = 0;
+        for (int index = n - 2; index >= 0; index--) {
+            if (nums[index] == 0) {
+                dp[index] = Integer.MAX_VALUE;
+                continue;
+            }
+            int minjump = Integer.MAX_VALUE;
+            for (int j = 1; j <= nums[index] && index + j < n; j++) {
+                int totalways = dp[index + j];
+                if (totalways != Integer.MAX_VALUE)
+                    minjump = Math.min(minjump, 1 + totalways);
+            }
+            dp[index] = minjump;
+        }
+        return dp[0];
+
     }
 
     public int solve(int[] nums, int index, int[] dp) {
