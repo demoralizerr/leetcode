1a2501af52163052e7bfe8c3d90e21b1b505850b
kartik patel <kartikpatell4july1997@gmail.com>
2026-05-21 11:24:01 +0530

    Time: 40 ms (93.37%), Space: 81.5 MB (55.29%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,26 +1,48 @@
 class Solution {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[10];
+    }
+
+    TrieNode root = new TrieNode();
+
+    public void insertInTrie(String num) {
+        TrieNode curr = root;
+        for (char ch : num.toCharArray()) {
+            int idx = ch - '0';
+            if (curr.children[idx] == null)
+                curr.children[idx] = new TrieNode();
+
+            curr = curr.children[idx];
+        }
+    }
+
+    public int findPrefix(int num) {
+        String str = String.valueOf(num);
+        TrieNode curr = root;
+        int len = 0;
+        for (char ch : str.toCharArray()) {
+            int idx = ch - '0';
+            if (curr.children[idx] == null)
+                break;
+
+            curr = curr.children[idx];
+            len++;
+        }
+        return len;
+
+    }
+
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        Set<String> s1 = new HashSet<>();
-          int max = 0;
+        int max = 0;
 
         for (int arr : arr1) {
-            String str = String.valueOf(arr);
-            int n = str.length();
-            for (int i = 0; i < n; i++) {
-                String prefix = str.substring(0, i + 1);
-                s1.add(prefix);
-            }
+            insertInTrie(String.valueOf(arr));
         }
 
         for (int arr : arr2) {
-            String str = String.valueOf(arr);
-            int n = str.length();
-            for (int i = 0; i < n; i++) {
-                String prefix = str.substring(0, i + 1);
-                if (s1.contains(prefix)) {
-                    max = Math.max(max, prefix.length());
-                }
-            }
+            int currlen = findPrefix(arr);
+            max = Math.max(max, currlen);
         }
 
         return max;
