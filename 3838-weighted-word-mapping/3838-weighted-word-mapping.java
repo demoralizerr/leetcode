513bcfa052d7ee09885d0bc209ad4a04ff011619
kartik patel <kartikpatell4july1997@gmail.com>
2026-06-13 11:41:53 +0530

    Time: 2 ms (96.85%), Space: 46.8 MB (14.02%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -1,7 +1,7 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
         StringBuilder res = new StringBuilder();
-        char[] alpha = new char[26];
+        
         for (String word : words) {
             int sum = 0;
             for (char ch : word.toCharArray()) {
